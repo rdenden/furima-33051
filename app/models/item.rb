6b6bc49af9c4e77a928d2a9036824db844945ac1
rdenden <dddrrr814@gmail.com>
2021-01-08 17:16:46 +0900

    商品詳細表示機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,7 +18,7 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category
+  belongs_to_active_hash :Category
   belongs_to_active_hash :Condition
   belongs_to_active_hash :Delivery_fee
   belongs_to_active_hash :Prefecture
