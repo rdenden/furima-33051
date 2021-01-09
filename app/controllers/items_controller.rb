4dd1ea379faa945115419b183de81962bdbbefa8
rdenden <dddrrr814@gmail.com>
2021-01-09 11:48:00 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
-
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_index, only: :edit
   def new
     @item = Item.new
   end
@@ -22,10 +22,29 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+  
+  def update
+    @item = Item.find(params[:id])
+     if @item.update(item_params)
+        redirect_to root_path
+     else render :edit
+     end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :image, :price, :comment, :category_id, :condition_id, :delivery_fee_id, :prefecture_id,
                                  :schedule_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    item = Item.find(params[:id])
+    unless item.user.id == current_user.id
+      redirect_to action: :index
+    end
   end
 end
