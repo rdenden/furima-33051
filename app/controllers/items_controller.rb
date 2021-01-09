d70268ab3fa03229517a1a8cf4768354b4bf8994
rdenden <dddrrr814@gmail.com>
2021-01-09 16:30:18 +0900

    ルーティングの記述を簡素化し、destroyアクションを修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -33,10 +33,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    if current_user.id == item.user_id
-      item.destroy
-      redirect_to root_path
+    @item = Item.find(params[:id])
+    if current_user.id == @item.user_id
+      if @item.destroy
+        redirect_to root_path
+      else render :edit
+      end
     end
   end
 
