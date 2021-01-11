8820c89951464c5231fb1d221eee027e1bad3806
rdenden <dddrrr814@gmail.com>
2021-01-11 17:16:55 +0900

    購入のためのフォーム修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,11 @@
 class OrdersController < ApplicationController
   def index
     @order_item = OrderItem.new
+    @item = Item. find(params[:item_id])
+    
+  end
+
+  def create
+  end
 
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,20 +1,21 @@
 class OrderItem
   include ActiveModel::Model
-  attr_accessor :user, :item, :buy, :postalcode_id, :prefecture_id, :municipality, :add, :building, :tel
+  attr_accessor :user, :item, :order, :postalcode_id, :prefecture_id, :municipality, :add, :building, :tel
   
   with_options presence: true do
     validates :postalcode_id, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }    
     validates :municipality
     validates :add
-    validates :tel , :format: { with: /\A\d{10,11}\z/ }
+    validates :tel , format: { with: /\A\d{10,11}\z/ }
+    validates :user
+    validates :item
+    validates :order
   end
-  validates :prefecture_id, :numericality: { other_than: 0 }
-  validates :user
-  varidates :item
-  varidates :buy
+  validates :prefecture_id, numericality: { other_than: 0 }
+  
 
   def save
-    Address.create(postalcode_id: postalcode_id, prefecture_id: prefecture_id, municipality: municipality, add: add, building: building)
+    Address.create(postalcode_id: postalcode_id, prefecture_id: prefecture_id, municipality: municipality, add: add, building: building, tel: tel)
     Buy.create(item_id: params[:id], user_id: current.user.id)
   end
 
