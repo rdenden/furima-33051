7233e367aeab5a49bfa1ca10133071e8a08b76dd
rdenden <dddrrr814@gmail.com>
2021-01-11 14:12:13 +0900

    購入のためのフォームを実装する

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: "items#index"
   resources :items do
     resources :order, only: [:index, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
