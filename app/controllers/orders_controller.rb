22fd1422802245f892fec65058dc5f426a848d2b
rdenden <dddrrr814@gmail.com>
2021-01-12 09:48:52 +0900

    購入のためのフォーム修正2

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,6 +6,23 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order_item = OrderItem.new(order_params)
+    
+     if @order_item.valid?
+       @order_item.save
+       redirect_to root_path
+     else
+       @item = Item. find(params[:item_id])
+       render action: :index
+     end
+  end
+    
+   
+  
+
+  private
+  def order_params
+    params.require(:order_item).permit(:postalcode_id, :prefecture_id, :municipality, :add, :building, :tel ).merge(item_id: params[:item_id], user_id: current_user.id )
   end
 
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,22 +1,24 @@
 class OrderItem
   include ActiveModel::Model
-  attr_accessor :user, :item, :order, :postalcode_id, :prefecture_id, :municipality, :add, :building, :tel
+  attr_accessor :user_id, :item_id, :postalcode_id, :prefecture_id, :municipality, :add, :building, :tel
   
   with_options presence: true do
     validates :postalcode_id, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }    
     validates :municipality
     validates :add
     validates :tel , format: { with: /\A\d{10,11}\z/ }
-    validates :user
-    validates :item
-    validates :order
+    validates :user_id
+    validates :item_id
   end
   validates :prefecture_id, numericality: { other_than: 0 }
   
 
   def save
-    Address.create(postalcode_id: postalcode_id, prefecture_id: prefecture_id, municipality: municipality, add: add, building: building, tel: tel)
-    Buy.create(item_id: params[:id], user_id: current.user.id)
+    
+    order = Order.create(item_id: item_id, user_id: user_id)
+    
+    Address.create(postalcode_id: postalcode_id, prefecture_id: prefecture_id, municipality: municipality, add: add, building: building, tel: tel, order_id: order.id)
+
   end
 
 
